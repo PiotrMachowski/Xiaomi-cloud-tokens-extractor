dc6e7d3d8f3d596e9992bbcfbb05e4fa541fd300
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2020-10-22 16:21:21 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,23 @@
+# Xiaomi Cloud Tokens Extractor
+[![buymeacoffee_badge](https://img.shields.io/badge/Donate-buymeacoffe-ff813f?style=flat)](https://www.buymeacoffee.com/PiotrMachowski)
+
+This tool/scripts retrieves tokens for all devices connected to Xiaomi cloud.
+
+## Windows
+Download and run [token_extractor.exe](https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/raw/master/token_extractor.exe).
+
+## Other platforms
+
+Install dependencies:
+```bash
+pip3 install pycryptodome pybase64 requests
+```
+
+Download and run script:
+```bash
+wget https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/raw/master/token_extractor.py
+python3 token_extractor.py
+```
+
+
+<a href="https://www.buymeacoffee.com/PiotrMachowski" target="_blank"><img src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
---
+++ get_tokens.py
@@ -0,0 +1,203 @@
+import base64
+import hashlib
+import hmac
+import json
+import os
+import time
+import requests
+import random
+from Crypto.Hash import MD5, SHA256
+
+
+class XiaomiCloudConnector:
+
+    def __init__(self, username, password, country):
+        self._username = username
+        self._password = password
+        self._country = country
+        self._agent = self.generate_agent()
+        self._device_id = self.generate_device_id()
+        self._session = requests.session()
+        self._sign = None
+        self._ssecurity = None
+        self._userId = None
+        self._cUserId = None
+        self._passToken = None
+        self._location = None
+        self._code = None
+        self._serviceToken = None
+
+    def login_step_1(self):
+        url = "https://account.xiaomi.com/pass/serviceLogin?sid=xiaomiio&_json=true"
+        headers = {
+            "User-Agent": self._agent,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        cookies = {
+            "userId": self._username
+        }
+        response = self._session.get(url, headers=headers, cookies=cookies)
+        valid = response.status_code == 200 and "_sign" in self.to_json(response.text)
+        if valid:
+            self._sign = self.to_json(response.text)["_sign"]
+        return valid
+
+    def login_step_2(self):
+        url = "https://account.xiaomi.com/pass/serviceLoginAuth2"
+        headers = {
+            "User-Agent": self._agent,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        fields = {
+            "sid": "xiaomiio",
+            "hash": (MD5.new(str.encode(self._password)).hexdigest() + "").upper(),
+            "callback": "https://sts.api.io.mi.com/sts",
+            "qs": "%3Fsid%3Dxiaomiio%26_json%3Dtrue",
+            "user": self._username,
+            "_sign": self._sign,
+            "_json": "true"
+        }
+        response = self._session.post(url, headers=headers, params=fields)
+        valid = response.status_code == 200 and "ssecurity" in self.to_json(response.text)
+        if valid:
+            json_resp = self.to_json(response.text)
+            self._ssecurity = json_resp["ssecurity"]
+            self._userId = json_resp["userId"]
+            self._cUserId = json_resp["cUserId"]
+            self._passToken = json_resp["passToken"]
+            self._location = json_resp["location"]
+            self._code = json_resp["code"]
+        return valid
+
+    def login_step_3(self):
+        headers = {
+            "User-Agent": self._agent,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        response = self._session.get(self._location, headers=headers)
+        if response.status_code == 200:
+            self._serviceToken = response.cookies.get("serviceToken")
+        return response.status_code == 200
+
+    def login(self):
+        self._session.cookies.set("sdkVersion", "accountsdk-18.8.15", domain="mi.com")
+        self._session.cookies.set("sdkVersion", "accountsdk-18.8.15", domain="xiaomi.com")
+        self._session.cookies.set("deviceId", self._device_id, domain="mi.com")
+        self._session.cookies.set("deviceId", self._device_id, domain="xiaomi.com")
+        if self.login_step_1():
+            if self.login_step_2():
+                if self.login_step_3():
+                    return True
+                else:
+                    print("Unable to get service token")
+            else:
+                print("Invalid login or password")
+        else:
+            print("Invalid username")
+        return False
+
+    def get_devices(self):
+        url = self.get_api_url() + "/home/device_list"
+        params = {
+            "data": '{"getVirtualModel":false,"getHuamiDevices":0}'
+        }
+        return self.execute_api_call(url, params)
+
+    def execute_api_call(self, url, params):
+        headers = {
+            "Accept-Encoding": "gzip",
+            "User-Agent": self._agent,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-xiaomi-protocal-flag-cli": "PROTOCAL-HTTP2"
+        }
+        cookies = {
+            "userId": str(self._userId),
+            "yetAnotherServiceToken": str(self._serviceToken),
+            "serviceToken": str(self._serviceToken),
+            "locale": "en_GB",
+            "timezone": "GMT+02:00",
+            "is_daylight": "1",
+            "dst_offset": "3600000",
+            "channel": "MI_APP_STORE"
+        }
+        millis = round(time.time() * 1000)
+        nonce = self.generate_nonce(millis)
+        signed_nonce = self.signed_nonce(nonce)
+        signature = self.generate_signature(url.replace("/app", ""), signed_nonce, nonce, params)
+        fields = {
+            "signature": signature,
+            "_nonce": nonce,
+            "data": params["data"]
+        }
+        response = self._session.post(url, headers=headers, cookies=cookies, params=fields)
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+    def get_api_url(self):
+        return "https://" + ("" if self._country == "cn" else (self._country + ".")) + "api.io.mi.com/app"
+
+    def signed_nonce(self, nonce):
+        hash_object = SHA256.new()
+        hash_object.update(base64.b64decode(self._ssecurity) + base64.b64decode(nonce))
+        return base64.b64encode(hash_object.digest()).decode('utf-8')
+
+    @staticmethod
+    def generate_nonce(millis):
+        nonce_bytes = os.urandom(8) + (int(millis / 60000)).to_bytes(4, byteorder='big')
+        return base64.b64encode(nonce_bytes).decode()
+
+    @staticmethod
+    def generate_agent():
+        agent_id = "".join(map(lambda i: chr(i), [random.randint(65, 69) for _ in range(13)]))
+        return f"Android-7.1.1-1.0.0-ONEPLUS A3010-136-{agent_id} APP/xiaomi.smarthome APPV/62830"
+
+    @staticmethod
+    def generate_device_id():
+        return "".join(map(lambda i: chr(i), [random.randint(97, 122) for _ in range(6)]))
+
+    @staticmethod
+    def generate_signature(url, signed_nonce, nonce, params):
+        signature_params = [url.split("com")[1], signed_nonce, nonce]
+        for k, v in params.items():
+            signature_params.append(f"{k}={v}")
+        signature_string = "&".join(signature_params)
+        signature = hmac.new(base64.b64decode(signed_nonce), msg=signature_string.encode(), digestmod=hashlib.sha256)
+        return base64.b64encode(signature.digest()).decode()
+
+    @staticmethod
+    def to_json(response_text):
+        return json.loads(response_text.replace("&&&START&&&", ""))
+
+
+print("Username (email or user ID):")
+username = input()
+print("Password:")
+password = input()
+print("Country (one of: ru, us, tw, sg, cn, de):")
+country = input()
+while country not in ["ru", "us", "tw", "sg", "cn", "de"]:
+    print("Invalid country provided. Valid values: ru, us, tw, sg, cn, de")
+    print("Country:")
+    country = input()
+
+connector = XiaomiCloudConnector(username, password, country)
+logged = connector.login()
+if logged:
+    devices = connector.get_devices()
+    if devices is not None:
+        print("Found devices:")
+        for device in devices["result"]["list"]:
+            print("-----")
+            if "name" in device:
+                print("NAME:  " + device["name"])
+            if "did" in device:
+                print("ID:    " + device["did"])
+            if "localip" in device:
+                print("IP:    " + device["localip"])
+            if "token" in device:
+                print("TOKEN: " + device["token"])
+    else:
+        print("Unable to get devices")
+else:
+    print("Unable to log in")
