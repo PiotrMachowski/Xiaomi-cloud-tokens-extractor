9da1931f9a644288e5c0f5b45773cab5580de61a
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2021-06-14 04:21:10 +0200

    Update run scripts

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-wget https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor_docker.zip
+wget -q https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor_docker.zip
 unzip token_extractor_docker.zip
 cd token_extractor_docker
 docker_image=$(docker build -q .)
-docker run --rm -rf $docker_image
+docker run --rm -it $docker_image
 docker rmi $docker_image
 cd ..
-rm -rf token_extractor_docker
+rm -rf token_extractor_docker token_extractor_docker.zip
