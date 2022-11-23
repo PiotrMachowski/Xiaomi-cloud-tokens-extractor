4ad2630beb81085a6defb538a4476a186ed1721d
Alex K <aleksandar.kasabov@ipsoft.com>
2022-11-23 13:44:04 +0100

    Remove wget from run scripts. Add bash failure options.
    Ref #75

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+set -o errexit  # fail on first error
+set -o nounset  # fail on undef var
+set -o pipefail # fail on first error in pipe
 
-wget -q https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor_docker.zip
+curl --silent --fail --show-error --location --remote-name --remote-header-name\
+  https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor_docker.zip
 unzip token_extractor_docker.zip
 cd token_extractor_docker
 docker_image=$(docker build -q .)
-docker run --rm -it $docker_image
+#docker run --rm -it $docker_image
 docker rmi $docker_image
 cd ..
 rm -rf token_extractor_docker token_extractor_docker.zip
